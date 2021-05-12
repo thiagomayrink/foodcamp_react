1ec725a284e5e994f8f775fa2d42693e776e896b
Bruna Topan <brutopanb@gmail.com>
2021-05-12 17:16:52 -0300

    Preparando review

--- src/topo.js
+++
@@ -1,9 +0,0 @@
-export default function Topo(props) {
-    const {titulo, subtitulo} = props;
-    return (
-        <div className="Topo">
-            <h1 className="titulo">{titulo}</h1>
-            <h2 className="subtitulo">{subtitulo}</h2>
-        </div>
-    );
-}
